1b52eda7c6e41285faf48d51b7aee1f452e01ea8
CHZS1 <109904048+CHZS1@users.noreply.github.com>
2022-11-21 17:39:50 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
-$(function() {
-    $(".tabs-content").not(":first").hide()
-    $(".tabs-head .tab").on("click", function() {
-        $(".tabs-head .tab").removeClass("active").eq($(this).index()).addClass("active")
-        $(".tabs-content").hide().eq($(this).index()).fadeIn()
-    }).eq(0).addClass("active")
-})
+
+$('.tabs-5').jQueryTab({
+    initialTab: 3,
+    tabInTransition: 'slideRightIn',
+    tabOutTransition: 'slideRightOut',
+    cookieName: 'active-tab-5'
+
+});
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
